772ea1fe0b727d2d58500cb28b80b4a7b3c1dc42
antonionq <antonioquijano@Antonios-MBP.attlocal.net>
2016-10-21 12:38:10 -0700

    custom page complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var app = angular.module('aaM', ['ui.router', 'uiGmapgoogle-maps']);
 
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $uiViewScrollProvider){
+
+	// app.run(function($rootScope) {
+	// 	$rootScope.$on('$stateChangeSuccess', function() {
+	// 		$document[0].body.scrollTop = $document[0].documentElement.scrollTop = 0;
+	// 	});
+	// });
+
+	$uiViewScrollProvider.useAnchorScroll();
+
+	app.run(function($rootScope,$location,$stateParams, $anchorScroll){
+  $rootScope.$on('$stateChangeSuccess', function(event, toState){
+    if($stateParams.scrollTo){
+      $location.hash($stateParams.scrollTo);
+      $anchorScroll();
+    }
+  });
+});
+
 
 	$urlRouterProvider.otherwise('/home');
   $stateProvider
@@ -19,12 +37,6 @@ app.config(function($stateProvider, $urlRouterProvider){
       url: '/services',
       templateUrl: '../templates/servicesView.html'
     })
-
-		// .state('contact', {
-    //   url: '/contact',
-    //   templateUrl: '../templates/contactView.html',
-		// 	controller: 'contactCtrl'
-    // })
 
 		.state('street', {
 			url: '/street',
